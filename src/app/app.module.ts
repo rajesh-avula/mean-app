13e57e658998348688b1faa25f0fefb39125c651
Rajesh <30767120+rajesh-avula@users.noreply.github.com>
2017-11-27 01:06:39 -0800

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {RouterModule } from '@angular/router';
+import {HttpModule} from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import {PostsService} from './posts.service';
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [PostsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
